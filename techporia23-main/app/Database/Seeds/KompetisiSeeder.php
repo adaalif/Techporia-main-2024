1a4aa9b30e96800f7118f070657f4d7e9d58fd8e
RistekFasilkom <fasilkomristek@gmail.com>
2024-07-30 20:52:05 +0700

    update

--- techporia23-main/app/Database/Seeds/KompetisiSeeder.php
+++ techporia23-main/app/Database/Seeds/KompetisiSeeder.php
@@ -14,9 +14,9 @@ class KompetisiSeeder extends Seeder
         ['id_kompetisi' => 3, 'nama_kompetisi' => 'UI/UX Design'],
         ['id_kompetisi' => 4, 'nama_kompetisi' => 'Essay'],
         ['id_kompetisi' => 5, 'nama_kompetisi' => 'Business Plan'],
-        ['id_kompetisi' => 6, 'nama_kompetisi' => 'Lukis'], // Added Painting competition
-        ['id_kompetisi' => 7, 'nama_kompetisi' => 'Tari'],    // Added Dance competition
-        ['id_kompetisi' => 8, 'nama_kompetisi' => 'Band'],       ];
+        ['id_kompetisi' => 6, 'nama_kompetisi' => 'Painting'], // Added Painting competition
+        ['id_kompetisi' => 7, 'nama_kompetisi' => 'Dance Competition'],    // Added Dance competition
+        ['id_kompetisi' => 8, 'nama_kompetisi' => 'Band/acoustic'],       ];
 
         $this->db->table('kompetisi')->insertBatch($data);
     }
